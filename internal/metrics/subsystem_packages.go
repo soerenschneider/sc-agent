0d8cff54d17a25b2b1a8148f535efa4d76394f1b
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-10-11 17:34:00 +0100

    refactor: add metrics

--- internal/metrics/subsystem_packages.go
+++ internal/metrics/subsystem_packages.go
@@ -20,6 +20,18 @@ var (
 		Name:      "updates_available_total",
 	})
 
+	UpdateCheckAvailableTimestamp = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Subsystem: packagesSubsystem,
+		Name:      "updates_available_check_timestamp_seconds",
+	})
+
+	PackagesInstalled = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Subsystem: packagesSubsystem,
+		Name:      "installed_total",
+	})
+
 	DnfErrors = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: namespace,
 		Subsystem: packagesSubsystem,
