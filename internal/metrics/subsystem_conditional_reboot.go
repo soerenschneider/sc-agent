7a2f56f9c43cbee90864ae4067b29d1a6856897e
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-10-26 18:08:34 +0100

    refactor: namespace metrics

--- internal/metrics/subsystem_conditional_reboot.go
+++ internal/metrics/subsystem_conditional_reboot.go
@@ -5,53 +5,60 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+const subsystemConditionalReboot = "conditional_reboot"
+
 var (
 	ProcessStartTime = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: namespace,
+		Subsystem: subsystemConditionalReboot,
 		Name:      "start_timestamp_seconds",
 	})
 
 	Heartbeat = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: namespace,
+		Subsystem: subsystemConditionalReboot,
 		Name:      "heartbeat_timestamp_seconds",
 	})
 
 	RebootIsPaused = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: namespace,
+		Subsystem: subsystemConditionalReboot,
 		Name:      "reboot_paused_bool",
 	})
 
 	Version = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: namespace,
+		Subsystem: subsystemConditionalReboot,
 		Name:      "version",
 	}, []string{"version"})
 
 	CheckerLastCheck = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: namespace,
-		Subsystem: "checker",
-		Name:      "last_check_timestamp_seconds",
+		Subsystem: subsystemConditionalReboot,
+		Name:      "checker_last_check_timestamp_seconds",
 	}, []string{"checker"})
 
 	CheckerState = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: namespace,
-		Subsystem: "checker",
-		Name:      "status",
+		Subsystem: subsystemConditionalReboot,
+		Name:      "checker_status",
 	}, []string{"checker", "status"})
 
 	AgentState = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: namespace,
-		Subsystem: "agent",
-		Name:      "state",
+		Subsystem: subsystemConditionalReboot,
+		Name:      "agent_state",
 	}, []string{"state", "checker"})
 
 	LastStateChange = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: namespace,
-		Subsystem: "agent",
-		Name:      "state_change_timestamp_seconds",
+		Subsystem: subsystemConditionalReboot,
+		Name:      "agent_state_change_timestamp_seconds",
 	}, []string{"state", "checker"})
 
 	RebootErrors = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: namespace,
-		Name:      "invocation_errors_total",
+		Subsystem: subsystemConditionalReboot,
+		Name:      "reboot_errors_total",
 	})
 )
