9e6f6b196f027974d0d13800f19369c5b6606da2
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-02-21 20:54:11 +0100

    feat: make sure data is always written to newly added pki backends

--- internal/services/components/pki/x509_repo/multi.go
+++ internal/services/components/pki/x509_repo/multi.go
@@ -4,6 +4,8 @@ import (
 	"crypto/x509"
 
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/soerenschneider/sc-agent/internal/storage"
 	pki2 "github.com/soerenschneider/sc-agent/pkg/pki"
 	"go.uber.org/multierr"
 )
@@ -20,9 +22,9 @@ func NewMultiKeyPairSink(sinks ...*KeyPairSink) (*MultiKeyPairSink, error) {
 	return &MultiKeyPairSink{sinks: sinks}, nil
 }
 
-func (f *MultiKeyPairSink) WriteCert(certData *pki2.CertData) error {
+func (fs *MultiKeyPairSink) WriteCert(certData *pki2.CertData) error {
 	var errs error
-	for _, sink := range f.sinks {
+	for _, sink := range fs.sinks {
 		if err := sink.WriteCert(certData); err != nil {
 			errs = multierr.Append(errs, err)
 		}
@@ -31,9 +33,39 @@ func (f *MultiKeyPairSink) WriteCert(certData *pki2.CertData) error {
 	return errs
 }
 
-func (f *MultiKeyPairSink) ReadCert() (*x509.Certificate, error) {
+// sanitize checks whether all configured storage backends contain data.
+// This is needed in cases where a new storage backend is added to the configuration after data was already
+// written to existing backends. The new backend would not receive existing data due to the way the Read() method
+// is implemented.
+func (fs *MultiKeyPairSink) sanitize() error {
+	certData := map[int]bool{}
+	for idx := range fs.sinks {
+		_, err := fs.sinks[idx].ReadCert()
+		if err == nil {
+			certData[idx] = true
+		}
+	}
+
+	if len(certData) > 0 && len(certData) != len(fs.sinks) {
+		log.Warn().Str("component", "storage").Msg("not all configured files are present")
+		return storage.ErrFilesMissing
+	}
+
+	return nil
+}
+
+func (fs *MultiKeyPairSink) ReadCert() (*x509.Certificate, error) {
+	// try to detect whether storages are configured that do not have any data written to them, yet
+	if err := fs.sanitize(); err != nil && errors.Is(err, storage.ErrFilesMissing) {
+		// due to inhomogeneity of written data and the combinations of possible storage configurations, it's
+		// hard to reconstruct the data from storage in order to write it to new storage backends.
+		// As this case is not going to happen often, we just request a new certificate and write it to all
+		// backends.
+		return nil, err
+	}
+
 	var errs error
-	for _, sink := range f.sinks {
+	for _, sink := range fs.sinks {
 		cert, err := sink.ReadCert()
 		if err == nil {
 			return cert, nil
