2b964f56fe9f0e8cef9d52451e1dde5cab023009
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-06-10 07:53:59 +0200

    refactor: drop kafka listener, rename files

---
+++ internal/services/components/system/system.go
@@ -0,0 +1,93 @@
+package system
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+	"github.com/soerenschneider/sc-agent/internal/config"
+	"github.com/soerenschneider/sc-agent/internal/domain/system"
+	"go.uber.org/multierr"
+)
+
+const serviceName = "machine"
+
+type MachineCmd struct {
+	useSudo bool
+}
+
+func New(conf config.PowerStatus) (*MachineCmd, error) {
+	return &MachineCmd{useSudo: conf.UseSudo}, nil
+}
+
+func (m *MachineCmd) SetCpuGovernor(governor system.Governor) error {
+	var errs error
+	for cpuIndex := 0; cpuIndex < runtime.NumCPU(); cpuIndex++ {
+		err := setCPUGovernor(cpuIndex, governor)
+		if err != nil {
+			multierr.Append(errs, err)
+		}
+	}
+	if errs != nil {
+		return fmt.Errorf("could not set cpu frequency: %w", errs)
+	}
+	return nil
+}
+
+func (m *MachineCmd) GetCurrentCpuGovernor() (string, error) {
+	governorPath := "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+
+	governor, err := os.ReadFile(governorPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read governor for CPU0: %v", err)
+	}
+
+	return strings.TrimSpace(string(governor)), nil
+}
+
+func setCPUGovernor(cpuIndex int, governor system.Governor) error {
+	cpuPath := fmt.Sprintf("/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor", cpuIndex)
+
+	if _, err := os.Stat(cpuPath); os.IsNotExist(err) {
+		return fmt.Errorf("CPU%d or cpufreq not supported on this system", cpuIndex)
+	}
+
+	return os.WriteFile(cpuPath, []byte(governor+"\n"), 0644)
+}
+
+func (m *MachineCmd) Shutdown() error {
+	cmd := []string{"systemctl", "shutdown"}
+
+	var c *exec.Cmd
+	if m.useSudo {
+		c = exec.Command("sudo", cmd...)
+	} else {
+		c = exec.Command(cmd[0], cmd[1:]...) // #nosec G204
+	}
+
+	if err := c.Run(); err != nil {
+		log.Error().Err(err).Str("service", serviceName).Msg("could not shutdown machine")
+		return err
+	}
+	return nil
+}
+
+func (m *MachineCmd) Reboot() error {
+	cmd := []string{"systemctl", "reboot"}
+
+	var c *exec.Cmd
+	if m.useSudo {
+		c = exec.Command("sudo", cmd...)
+	} else {
+		c = exec.Command(cmd[0], cmd[1:]...) // #nosec G204
+	}
+
+	if err := c.Run(); err != nil {
+		log.Error().Err(err).Str("service", serviceName).Msg("could not reboot machine")
+		return err
+	}
+	return nil
+}
